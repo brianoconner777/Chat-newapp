5934737b3115e9743713a5d1cc547c9d1668c5eb
Saurabh Garg <saurabh.garg5991@gmail.com>
2017-10-10 15:28:03 +0530

    sockets made and tested

---
+++ server.js
@@ -0,0 +1,75 @@
+//	Customization
+
+var appPort = 16558;
+
+// Librairies
+
+var express = require('express'), app = express();
+var http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+
+var jade = require('jade');
+// var io = require('socket.io').listen(app);
+var pseudoArray = ['admin']; //block the admin username (you can disable it)
+
+// Views Options
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false });
+
+app.use(express.static(__dirname + '/public'));
+
+// Render and send the main page
+
+app.get('/', function(req, res){
+  res.render('home.jade');
+});
+server.listen(appPort);
+// app.listen(appPort);
+console.log("Server listening on port " + appPort);
+
+// Handle the socket.io connections
+
+var users = 0; //count the users
+
+io.sockets.on('connection', function (socket) { // First connection
+	users += 1; // Add 1 to the count
+	reloadUsers(); // Send the count to all the users
+	socket.on('message', function (data) { // Broadcast the message to all
+		if(pseudoSet(socket))
+		{
+			var transmit = {date : new Date().toISOString(), pseudo : socket.nickname, message : data};
+			socket.broadcast.emit('message', transmit);
+			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
+		}
+	});
+	socket.on('setPseudo', function (data) { // Assign a name to the user
+		if (pseudoArray.indexOf(data) == -1) // Test if the name is already taken
+		{
+			pseudoArray.push(data);
+			socket.nickname = data;
+			socket.emit('pseudoStatus', 'ok');
+			console.log("user " + data + " connected");
+		}
+		else
+		{
+			socket.emit('pseudoStatus', 'error') // Send the error
+		}
+	});
+	socket.on('disconnect', function () { // Disconnection of the client
+		users -= 1;
+		reloadUsers();
+		if (pseudoSet(socket))
+		{
+			console.log("disconnect...");
+			var pseudo;
+			pseudo = socket.nickname;
+			var index = pseudoArray.indexOf(pseudo);
+			pseudo.slice(index - 1, 1);
+		}
+	});
+});
+
